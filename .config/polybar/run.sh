db9edfb59be6211ce67c8dd089858581c12d5d09
Enrico lerrigatto La Cava <me@lerrigatto.com>
2021-04-28 11:57:53 +0200

    polybar: add polybar and tomato timer

---
+++ .config/polybar/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Run on all the monitors
+#for monitor in $(polybar --list-monitors | cut -d":" -f1); do
+  echo "---" | tee -a /tmp/polybar-bottom1.log
+  MONITOR=$monitor polybar --reload bottom1 2>&1 | tee -a /tmp/polybar-bottom1.log & disown
+#done
+echo "Bars launched..."
