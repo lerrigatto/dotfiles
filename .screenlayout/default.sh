12ebf85a77c145be44b71a33ffa2543e04ceb134
Enrico lerrigatto La Cava <me@lerrigatto.com>
2021-06-09 15:02:23 +0000

    remove screenlayout

--- .screenlayout/default.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DVI-I-1 --off --output DVI-I-0 --off --output DP-1 --mode 1920x1080 --pos 2560x200 --rotate normal --output DP-0 --off
