517f1118ffe7a1d0d550f9003012de486cecfdbe
Enrico lerrigatto La Cava <me@lerrigatto.com>
2021-11-15 22:02:31 +0100

    fish: add git branch to prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,5 @@
 function fish_prompt
-  echo -s (set_color blue) (__kube_prompt) (set_color $fish_color_cwd) " " (prompt_pwd) (set_color normal) "> "
+  set -g __fish_git_prompt_showcolorhints true
+  set -g __fish_git_prompt_shorten_branch_len 25
+  echo -s (set_color blue) (__kube_prompt) (set_color $fish_color_cwd) (fish_git_prompt) " " (prompt_pwd) (set_color normal) "> "
 end
